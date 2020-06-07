dbc7487e2a44739abcd98419dc5560491e7a5654
Sujan Dhakal <dhakals@lafayette.edu>
2020-06-07 01:08:01 -0400

    quote api

--- script/script_quote.js
+++ script/script_quote.js
@@ -3,17 +3,16 @@ function displayQuote(){
 	xhttp.onreadystatechange = function() {					
 		if (this.readyState == 4 && this.status == 200) {
 			var obj = JSON.parse(this.responseText);
-			var quote = obj.contents.quotes[0].quote;
-			var author = obj.contents.quotes[0].author;
+			var quote = obj.quote.quoteText;
+			var author = obj.quote.quoteAuthor;
 			document.getElementById("content").innerHTML=quote;
 			document.getElementById("author").innerHTML=author;
 		}
 	};
 
-	const url= "https://quotes.rest/qod?language=en";
+	const url= "https://quote-garden.herokuapp.com/api/v2/quotes/random";
 	xhttp.open("GET", url, true);
 	xhttp.send(null);
 }
 
 document.getElementById("more").addEventListener("click", displayQuote);
-
